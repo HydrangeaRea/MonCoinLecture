52cf147b240af452eb0475c5b0462aada8713b87
Lea <lea.kolodziejczak@gmail.com>
2023-01-06 12:16:35 +0100

    premier commit

---
+++ src/test/java/com/example/MonCoinLecture/MonCoinLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MonCoinLecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonCoinLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
