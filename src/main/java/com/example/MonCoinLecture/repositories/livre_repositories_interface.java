c945bdbdebe0163030ab064ed547b293802752e4
robin <robinwaegaert@gmail.com>
2023-01-06 17:21:53 +0100

    ajout livre_repositories

---
+++ src/main/java/com/example/MonCoinLecture/repositories/livre_repositories_interface.java
@@ -0,0 +1,7 @@
+package com.example.MonCoinLecture.repositories;
+
+import com.example.MonCoinLecture.beans.livre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface livre_repositories_interface extends JpaRepository<livre, Integer> {
+}
