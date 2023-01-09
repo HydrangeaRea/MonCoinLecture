482a616af3bb4fb3f18bf6023caed21fa8cc7b28
robin <robinwaegaert@gmail.com>
2023-01-09 09:44:36 +0100

    ajout livre et avis_repositories

---
+++ src/main/java/com/example/MonCoinLecture/repositories/livres_repositories_interface.java
@@ -0,0 +1,7 @@
+package com.example.MonCoinLecture.repositories;
+
+import com.example.MonCoinLecture.beans.Livres;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface livres_repositories_interface extends JpaRepository<Livres, Integer> {
+}
