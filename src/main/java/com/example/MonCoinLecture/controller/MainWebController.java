a65c6856d7c57f2ea36639b8ca7da114478e9b7a
robin <robinwaegaert@gmail.com>
2023-01-09 15:05:32 +0100

    ajout début partie Avis

--- src/main/java/com/example/MonCoinLecture/controller/MainWebController.java
+++ src/main/java/com/example/MonCoinLecture/controller/MainWebController.java
@@ -1,10 +1,33 @@
 package com.example.MonCoinLecture.controller;
 
+import com.example.MonCoinLecture.beans.Avis;
+import com.example.MonCoinLecture.beans.Utilisateurs;
+import com.example.MonCoinLecture.beans.Livres;
 
+import com.example.MonCoinLecture.repositories.avisRepositoriesInterface;
+import com.example.MonCoinLecture.repositories.livresRepositoriesInterface;
+import com.example.MonCoinLecture.repositories.utilisateursRepositoriesInterface;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/API")
 public class MainWebController {
+
+    @Autowired
+    private livresRepositoriesInterface livresRepositoriesInterface;
+    @Autowired
+    private  utilisateursRepositoriesInterface utilisateursRepositoriesInterface;
+    @Autowired
+    private avisRepositoriesInterface avisRepositoriesInterface;
+
+    @GetMapping("/ajouterAvis)")
+    public String Ajouter_Avis(@RequestBody Avis avis1) {
+            avisRepositoriesInterface.save(avis1);
+            return "OK";
+    }
 }
---
+++ src/main/resources/static/test.js
@@ -0,0 +1,23 @@
+function getValue() {
+    var input_nom_membre = document.getElementById("input_nom_membre").value;
+    var input_nom_livre = document.getElementById("input_nom_livre").value;
+    var input_avancement = document.getElementById("avancement").value;
+    var input_note = document.getElementById("input_note").value;
+    var input_commentaire= document.getElementById("input_commentaire").value;
+    var input_suggestion_livre = document.getElementById("input_suggestion_livre").value;
+    var test = document.getElementById("phrase");
+
+
+    let avis1= {avancement: avancement, commentaire: input_commentaire, id_livre: input_nom_livre, id_livre_suggestion: input_suggestion_livre, id_utilisateur: input_nom_membre, note: input_note}
+    test.innerHTML= avis1;
+    $.ajax({
+        type: "POST",
+        headers: {"Content-type" : "application/json"},
+        url: "http://localhost:8080/API/ajouterAvis",
+        data: JSON.stringify(avis1),
+        success: function(avis1) {
+            alert("L'API m'a retourné "+ avis1);
+            console.log(avis1);
+        }
+    });
+}
