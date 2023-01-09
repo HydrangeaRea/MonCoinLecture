12fe8aa9e346ace38a9381652b84cea69dbc47ad
robin <robinwaegaert@gmail.com>
2023-01-09 09:41:44 +0100

    ajout utilisateur_repositories

--- src/main/java/com/example/MonCoinLecture/repositories/utilisateur_repositories_interface.java
+++
@@ -1,7 +0,0 @@
-package com.example.MonCoinLecture.repositories;
-
-import com.example.MonCoinLecture.beans.utilisateur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface utilisateur_repositories_interface extends JpaRepository<utilisateur, Integer> {
-}
