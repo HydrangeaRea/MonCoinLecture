0174c2dd7db2e9d08d824c38855e72ecd5caab48
robin <robinwaegaert@gmail.com>
2023-01-09 09:40:52 +0100

    ajout utilisateur_repositories

---
+++ src/main/java/com/example/MonCoinLecture/repositories/avis_repositories_interface.java
@@ -0,0 +1,7 @@
+package com.example.MonCoinLecture.repositories;
+
+import com.example.MonCoinLecture.beans.Avis;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface avis_repositories_interface extends JpaRepository<Avis, Integer> {
+}
