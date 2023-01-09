7628de18ad1894b20c3d35826024e2d6ff8a7efe
lea <lea.kolodziejczak@gmail.com>
2023-01-09 14:01:11 +0100

    Ajout page livre, head

--- src/main/resources/static/js/scriptEnCommun.js
+++ src/main/resources/static/js/scriptEnCommun.js
@@ -2,3 +2,11 @@
 Le header, le footer, la nav barre et l'aside membre
 comme ça on a à le coder une seule fois
 */
+
+$.get("head.html", function (data) {
+    $(".head-placeholder").replaceWith(data);
+});
+
+$.get("membre.html", function (data) {
+    $(".membre-placeholder").replaceWith(data);
+});
