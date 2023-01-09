f88a185d62785c5ef5f55aea2f28190604804833
robin <robinwaegaert@gmail.com>
2023-01-09 09:54:56 +0100

    ajout MainWebControoler

---
+++ src/main/java/com/example/MonCoinLecture/controller/MainWebController.java
@@ -0,0 +1,10 @@
+package com.example.MonCoinLecture.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/API")
+public class MainWebController {
+}
