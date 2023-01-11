cfe8d62745089e0883be62dd07ab7c3ef5681737
evaco <eva.commercon@gmail.com>
2023-01-11 11:39:25 +0100

    modif script en commun et membre

--- src/main/resources/static/js/scriptEnCommun.js
+++ src/main/resources/static/js/scriptEnCommun.js
@@ -3,10 +3,23 @@ Le header, le footer, la nav barre et l'aside membre
 comme ça on a à le coder une seule fois
 */
 
-$.get("head.html", function (data) {
-    $(".head-placeholder").replaceWith(data);
-});
+$(".head-placeholder").replaceWith(data);
 
-$.get("membre.html", function (data) {
-    $(".membre-placeholder").replaceWith(data);
-});
+
+
+$(".membre-placeholder").replaceWith(data);
+
+
+let data2 = {pseudo: sessionStorage.getItem('pseudo')};
+console.log(data2);
+alert("cucou");
+        $.ajax({
+            type: "POST",
+            headers: {"Content-Type": "application/JSON"},
+            url: "http://localhost:8080/API/mesInformations",
+            data: JSON.stringify(data),
+            success: function(resultat) {
+                console.log(resultat.pseudo);
+              $('.pseudo-membre').html(resultat.pseudo);
+            }
+            });
