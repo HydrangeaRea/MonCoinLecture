d1ecbbe833029ae652e1c32c625a88bdfa5a3973
evaco <eva.commercon@gmail.com>
2023-01-11 12:12:17 +0100

    modif script en commun et membre = affichage infos membre à gauche

--- src/main/resources/static/js/scriptEnCommun.js
+++ src/main/resources/static/js/scriptEnCommun.js
@@ -3,23 +3,27 @@ Le header, le footer, la nav barre et l'aside membre
 comme ça on a à le coder une seule fois
 */
 
-$(".head-placeholder").replaceWith(data);
 
+$.get("head.html", function (data) {
+    $(".head-placeholder").replaceWith(data);
+});
 
-
-$(".membre-placeholder").replaceWith(data);
-
+$.get("membre.html", function (data) {
+    $(".membre-placeholder").replaceWith(data);
+});
 
 let data2 = {pseudo: sessionStorage.getItem('pseudo')};
-console.log(data2);
-alert("cucou");
+
         $.ajax({
             type: "POST",
             headers: {"Content-Type": "application/JSON"},
             url: "http://localhost:8080/API/mesInformations",
-            data: JSON.stringify(data),
+            data: JSON.stringify(data2),
             success: function(resultat) {
-                console.log(resultat.pseudo);
               $('.pseudo-membre').html(resultat.pseudo);
+              $('.date-inscription').html("Inscrit depuis le : "+resultat.dateInscription);
+              $('.auteur-fav').html("Auteur favori : <br>"+resultat.auteurFavori);
+              $('.livre-fav').html("Livre favori : <br>"+resultat.livreFavori);
             }
             });
+
