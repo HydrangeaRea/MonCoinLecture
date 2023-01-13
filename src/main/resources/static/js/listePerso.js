2e316bc74ae348a32426e6811cc7f5f25a520ed9
evaco <eva.commercon@gmail.com>
2023-01-13 12:05:50 +0100

    modifs commentaire dans liste d'avis

--- src/main/resources/static/js/listePerso.js
+++ src/main/resources/static/js/listePerso.js
@@ -13,8 +13,8 @@ console.log(retour);
             '<td id="titre-livre'+(i+1)+'">Titre du livre</td>' +
             '<td id="avancement-livre'+(i+1)+'">lu</td>' +
             '<td id="avis'+(i+1)+'">5/5</td>' +
-            '<td id="commentaire'+(i+1)+'"> <button class="afficher-commentaire">Commentaire</button><br>' +
-            '<button id="bouton-livre'+(i+1)+'">Fiche du livre</button><br>'
+            '<td id="commentaire'+(i+1)+'">Mon commentaire</td>' +
+            '<td><button id="bouton-livre'+(i+1)+'">Fiche du livre</button><br>'
 
         )
     };
@@ -29,6 +29,7 @@ console.log(retour);
         $("#titre-livre" + i).html(retour[(i - 1)].livre.titre);
         $("#avancement-livre" + i).html(retour[(i - 1)].avancement);
         $("#avis" + i).html(retour[(i - 1)].note+"/5");
+        $("#commentaire" + i).html(retour[(i - 1)].commentaire);
         $("#bouton-livre" + i).html('<a href="livre.html?ouvrage='+retour[(i-1)].livre.titre+'&">Fiche du livre</a>');
 
     }; 
